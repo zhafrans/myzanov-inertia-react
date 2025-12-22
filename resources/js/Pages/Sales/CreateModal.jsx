53e39cc97e2e06e7762d7b19302e92b46cd9bdf2
zhafrans <zhafransiswanto31@gmail.com>
2025-12-22 13:57:00 +0700

    update placeholder

--- resources/js/Pages/Sales/CreateModal.jsx
+++ resources/js/Pages/Sales/CreateModal.jsx
@@ -401,7 +401,10 @@ export default function CreateModal() {
     return (
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>
-                <Button size="sm" className="w-full md:w-auto text-xs md:text-sm">
+                <Button
+                    size="sm"
+                    className="w-full md:w-auto text-xs md:text-sm"
+                >
                     <Plus className="w-3 h-3 md:w-4 md:h-4 mr-1 md:mr-2" />
                     <span className="hidden sm:inline">Tambah Data</span>
                     <span className="sm:hidden">Tambah</span>
@@ -736,7 +739,7 @@ export default function CreateModal() {
                                     name="card_number"
                                     value={form.card_number}
                                     onChange={handleChange}
-                                    placeholder="Opsional"
+                                    placeholder="Input no kartu"
                                     className={
                                         errors.card_number
                                             ? "border-red-500"
