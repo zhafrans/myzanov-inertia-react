f471abf16d33eecd2a74b3482902583b4e077dba
zhafrans <zhafransiswanto31@gmail.com>
2025-12-24 15:12:10 +0700

    update show sales

--- resources/js/Pages/Sales/Show.jsx
+++ resources/js/Pages/Sales/Show.jsx
@@ -92,7 +92,7 @@ export default function SalesShow() {
                         </Button>
                     )}
 
-                    <Button 
+                    {/* <Button 
                         onClick={handlePrint} 
                         variant="outline"
                         size="sm"
@@ -100,7 +100,7 @@ export default function SalesShow() {
                     >
                         <Printer className="w-3 h-3 md:w-4 md:h-4 mr-1 md:mr-2" />
                         Print
-                    </Button>
+                    </Button> */}
 
                     {!sale.is_lunas && canInputInstallment(user) && (
                         <Button
